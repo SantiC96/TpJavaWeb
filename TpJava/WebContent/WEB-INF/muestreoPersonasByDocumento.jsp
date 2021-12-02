17c2a434101f70d35ec364d935e56eba15067b97
SantiC96 <santiago.cancio96@gmail.com>
2021-12-02 12:23:23 -0300

    Push servlets y JSPs

---
+++ TpJava/WebContent/WEB-INF/muestreoPersonasByDocumento.jsp
@@ -0,0 +1,42 @@
+<%@page import="entidades.Persona"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+
+<%Persona per = (Persona)request.getAttribute("persona"); %>
+<title>Datos de la persona</title>
+</head>
+<body>
+<p class="fs-2 font-monospace" align="center" >Datos de <%System.out.println(" "+ per.getNombre()+ " "+ per.getApellido()); %></p>
+	<fieldset>
+		<table class="table table-hover table-striped table-sm border-secondary border border-5 rounded-3">	
+		  <thead  class="table-dark">
+		    <tr>
+		      <th scope="col">DNI</th>
+		      <th scope="col">Nombre</th>
+		      <th scope="col">Apellido</th>
+		      <th scope="col">Telefono</th>
+		      <th scope="col">Oficio</th>
+		      <th scope="col">Area de Trabajo</th>
+		      <th scope="col">Valuación Promedio</th>
+		      <td scope="col" align="center""><a href="index.html"><button type="button" class="btn btn-light"  style="width:100px; height:35px;r">Volver</button></a></td>
+		    </tr>
+		  </thead>
+		  <tbody>
+			<tr>
+				<td><%=per.getDni() %></td>
+				<td><%=per.getNombre() %></td>
+				<td><%=per.getApellido() %></td>
+				<td><%=per.getTelefono() %></td>
+				<td><%=per.getOficio() %></td>
+				<td><%=per.getAreaTrabajo() %></td>
+				<td><%=per.getValuacionPromedio() %></td>
+			</tr>
+			</tbody>
+		</table>
+	</fieldset>
+</body>
+</html>
---
+++ TpJava/src/servlets/servletPersonasByDocumento.java
@@ -0,0 +1,50 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import conexionSQL.DataPersona;
+import entidades.Persona;
+
+/**
+ * Servlet implementation class servletPersonasByDocumento
+ */
+@WebServlet("/servletPersonasByDocumento")
+public class servletPersonasByDocumento extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public servletPersonasByDocumento() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		Persona per = new Persona();
+		DataPersona dp = new DataPersona();
+		per = dp.getByDocumento(per);
+		request.setAttribute("persona", per);
+		request.getRequestDispatcher("WEB-INF/muestreoPersonasByDocumento.jsp").forward(request, response);
+	}
+
+}
